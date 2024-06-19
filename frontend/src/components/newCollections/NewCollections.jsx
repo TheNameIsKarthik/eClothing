e6fa5d73ddb6aab33bb6486ffb6750ef094de655
Karthik <thenameiskarthik@pm.me>
2024-06-19 14:54:09 +0530

    Update NewCollections.jsx

--- frontend/src/components/newCollections/NewCollections.jsx
+++ frontend/src/components/newCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection, setNew_collection] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4000/newcollections")
+    fetch("https://eclothing-backend.onrender.com/newcollections")
       .then((res) => res.json())
       .then((data) => setNew_collection(data));
   }, []);
