bc0a9ae161480c4375c35911a5ea521d9edcac5d
Karthik <thenameiskarthik@pm.me>
2024-06-19 14:53:26 +0530

    Update ListProduct.jsx

--- admin/src/components/ListProduct/ListProduct.jsx
+++ admin/src/components/ListProduct/ListProduct.jsx
@@ -8,7 +8,7 @@ const ListProduct = () => {
   const [allProducts, setAllProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch("https://eclothing-backend.onrender.com/allproducts")
       .then((resp) => resp.json())
       .then((data) => {
         setAllProducts(data);
@@ -20,7 +20,7 @@ const ListProduct = () => {
   }, []);
 
   const remove_product = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch("https://eclothing-backend.onrender.com/removeproduct", {
       method: "POST",
       headers: {
         Accept: "application/json",
