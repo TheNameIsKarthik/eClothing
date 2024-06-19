07a83d6d32ce109a9972855c50c823b5d14d3026
Karthik <thenameiskarthik@pm.me>
2024-06-19 14:53:04 +0530

    Update AddProduct.jsx

--- admin/src/components/AddProduct/AddProduct.jsx
+++ admin/src/components/AddProduct/AddProduct.jsx
@@ -29,7 +29,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append("product", image);
 
-    await fetch("http://localhost:4000/upload", {
+    await fetch("https://eclothing-backend.onrender.com/upload", {
       method: "POST",
       headers: {
         Accept: "application/json",
@@ -45,7 +45,7 @@ const AddProduct = () => {
       product.image = responseData.image_url;
       console.log(product);
 
-      await fetch("http://localhost:4000/addproduct", {
+      await fetch("https://eclothing-backend.onrender.com/addproduct", {
         method: "POST",
         headers: {
           Accept: "application/json",
